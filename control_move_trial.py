701da32f9b3b7b6cd721ead4d6597a2cd69e03df
Melissa Kazazic <mkazazic01@gmail.com>
2021-11-02 22:30:41 -0400

    fix: include left joystick y inputs for slower speeds

--- control_move_trial.py
+++ control_move_trial.py
@@ -29,9 +29,10 @@ def control_move(x,y,a,b,rb,rt):
                round(left_speed), round(right_speed)
     if a:
         left_speed = MOTOR_MID_SPEED + (x*MOTOR_INT_SPEED)
-        left_speed = left_speed + abs(y)*(MOTOR_TOP_SPEED-left_speed)
         right_speed = MOTOR_MID_SPEED - (x*MOTOR_INT_SPEED)
-        right_speed = right_speed + abs(y)*(MOTOR_TOP_SPEED-right_speed)
+        increase_fwd = y*(MOTOR_TOP_SPEED-max(left_speed, right_speed))
+        left_speed = left_speed + increase_fwd
+        right_speed = right_speed + increase_fwd
     if b:
         left_speed = 0
         right_speed = 0
