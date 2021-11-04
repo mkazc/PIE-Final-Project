9305a68a627e7021f1b15fb5f933253250a6e87b
Melissa Kazazic <mkazazic01@gmail.com>
2021-11-03 21:36:17 -0400

    fix: reclean code

--- control_move_trial/control_move_trial.ino
+++ control_move_trial/control_move_trial.ino
@@ -16,23 +16,20 @@ int rightMotorSpeed=0;
 
 // Set up data recieve and use
 String readString = "";
-String data;
-char lastIncomingString;
+char lastIncomingChar;
 int ind1;
 String left_motors = "";
 String right_motors = "";
-String left_rev = "";
-String right_rev = "";
 
 
 void setup() {
   // put your setup code here, to run once:
   AFMS.begin();
   // Set initial motor speeds
-  leftMotor1->setSpeed(0);
-  leftMotor2->setSpeed(0);
-  rightMotor1->setSpeed(0);
-  rightMotor2->setSpeed(0);
+  leftMotor1->setSpeed(leftMotorSpeed);
+  leftMotor2->setSpeed(leftMotorSpeed);
+  rightMotor1->setSpeed(rightMotorSpeed);
+  rightMotor2->setSpeed(rightMotorSpeed);
   // Stop the motors
   leftMotor1->run(RELEASE);
   leftMotor2->run(RELEASE);
@@ -45,8 +42,8 @@ void setup() {
 void loop() {
   while(true) {
     if (Serial.available() > 0) {
-          lastIncomingString = Serial.read();
-          if(lastIncomingString == '*'){
+          lastIncomingChar = Serial.read();
+          if(lastIncomingChar == '*'){
             ind1 = readString.indexOf(',');
             left_motors = readString.substring(0,ind1);
             right_motors = readString.substring(ind1+1);
@@ -64,9 +61,9 @@ void loop() {
             rightMotor2->run(FORWARD);
             readString = "";
           }
-    else{
-      readString += lastIncomingString;
-    }
+          else{
+            readString += lastIncomingChar;
+          }
     }
   }
 }
