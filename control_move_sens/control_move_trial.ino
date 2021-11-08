8c1fac66cb3c8bbd0f7ae089d4a9483c03c8615e
Jaclyn Ho <jho2@olin.edu>
2021-11-08 10:55:42 -0500

    Copy of control move trial for me to dick around with

---
+++ control_move_sens/control_move_trial.ino
@@ -0,0 +1,79 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+// Select which 'port' M1, M2, M3 or M4. In this case, M1
+Adafruit_DCMotor *leftMotor1 = AFMS.getMotor(4);
+Adafruit_DCMotor *leftMotor2 = AFMS.getMotor(1);
+Adafruit_DCMotor *rightMotor1 = AFMS.getMotor(3);
+Adafruit_DCMotor *rightMotor2 = AFMS.getMotor(2);
+
+int leftMotorSpeed=0;
+int rightMotorSpeed=0;
+
+// Set up data recieve and use
+String readString = "";
+char lastIncomingChar;
+bool begin = false;
+int ind0;
+int ind1;
+int ind2;
+String left_motors = "";
+String right_motors = "";
+
+
+void setup() {
+  // put your setup code here, to run once:
+  AFMS.begin();
+  // Set initial motor speeds
+  leftMotor1->setSpeed(leftMotorSpeed);
+  leftMotor2->setSpeed(leftMotorSpeed);
+  rightMotor1->setSpeed(rightMotorSpeed);
+  rightMotor2->setSpeed(rightMotorSpeed);
+  // Stop the motors, set to run forward
+  // NOTE: Don't call often, will make wheels jitter
+  leftMotor1->run(FORWARD);
+  leftMotor2->run(FORWARD);
+  rightMotor1->run(FORWARD);
+  rightMotor2->run(FORWARD);
+  // Set up serial
+  Serial.begin(9600);
+}
+
+void loop() {
+  while(true) {
+    // Make sure data is at start
+    if (!begin){
+      lastIncomingChar = Serial.read(); // grab the most recent char
+      // when to start through ternary operator, make sure data format usable
+      begin = ( lastIncomingChar == '.' ) ? true : false;
+    }
+    // Make sure that there is new data over Serial
+    if ((Serial.available() > 0) && (begin)) {
+          lastIncomingChar = Serial.read(); // grab the most recent char
+          // run when final index sent
+          if(lastIncomingChar == '*') {
+            ind0 = readString.indexOf('.'); // find initial index value
+            ind1 = readString.indexOf(','); // find index value of ',' in string
+            left_motors = readString.substring(ind0+1,ind1); // grab string from index 0 to ','
+            right_motors = readString.substring(ind1+1); // grab string from index ',' on
+            leftMotorSpeed = left_motors.toInt(); // get integer value and store
+            rightMotorSpeed = right_motors.toInt();
+            // change speed
+            leftMotor1->setSpeed(leftMotorSpeed);
+            leftMotor2->setSpeed(leftMotorSpeed);
+            rightMotor1->setSpeed(rightMotorSpeed);
+            rightMotor2->setSpeed(rightMotorSpeed);
+            // clear string to reuse
+            readString = "";
+          }
+          else {
+            // build string starting from '.' until '*' char is reached
+            readString += lastIncomingChar;
+          }
+    }
+  }
+}
---
+++ control_move_sens/control_move_trial.py
@@ -0,0 +1,40 @@
+import math
+import threading
+
+MOTOR_TOP_SPEED = 50
+MOTOR_MID_SPEED = MOTOR_TOP_SPEED/2
+MOTOR_INT_SPEED = MOTOR_TOP_SPEED/4
+
+def control_move(x,y,a,b,rb,rt):
+    '''
+    Takes in controller inputs and outputs motor speeds.
+
+    Inputs:
+        x: (-1 to 1) Left Joystick X tilt
+        y: (-1 to 1) Left Joystick Y tilt
+        a: (0 or 1) A button pressed
+        b: (0 or 1) B button pressed
+        rb: (0 or 1) Right bumper pressed
+        rt: (0 to 1) Right trigger analog
+    '''
+    left_speed = 0
+    right_speed = 0
+    # spin in place
+    if rb:
+        if (x < 0):
+            right_speed = abs(x)*MOTOR_TOP_SPEED
+        else:
+            left_speed = abs(x)*MOTOR_TOP_SPEED
+        return [round(left_speed), round(right_speed)]
+    # if "a" button pressed, go forward (with difference in X and Y)
+    if a:
+        left_speed = MOTOR_MID_SPEED + (x*MOTOR_INT_SPEED)
+        right_speed = MOTOR_MID_SPEED - (x*MOTOR_INT_SPEED)
+        increase_fwd = y*(MOTOR_TOP_SPEED-max(left_speed, right_speed))
+        left_speed = left_speed + increase_fwd
+        right_speed = right_speed + increase_fwd
+    # if "b" button pressed, stop both motors
+    if b:
+        left_speed = 0
+        right_speed = 0
+    return [round(left_speed), round(right_speed)]
