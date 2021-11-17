6c8fc621e7864b7904616c53cfe3682f7fe24c04
Melissa Kazazic <mkazazic01@gmail.com>
2021-11-16 22:31:38 -0500

    feat: complete apache server files to be tested

--- pi-cam-vr/video-manipulation.js
+++ pi-cam-vr/video-manipulation.js
@@ -1,3 +1,13 @@
+var scene,
+    camera,
+    renderer,
+    element,
+    container,
+    effect,
+    video,
+    canvas,
+    context;
+
 init();
 
 function init(){
@@ -14,30 +24,81 @@ function init(){
     container.appendChild(element);
     // Add VR sterescopic effect
     effect = new THREE.StereoEffect(renderer);
-    // Set up Orbit controls for touch/click on web
-    controls = new THREE.OrbitControls(camera, element);
-    controls.target.set(
-        camera.position.x + 0.15,
-        camera.position.y,
-        camera.position.z
-    )
-    // Turn off pan and zoom
-    controls.noPan = true;
-    controls.noZoom = true;
-    // Setup device orientation event listener
-    window.addEventListener('deviceorientation', setOrientationControls, true);
-    // Setup when device supports device orientation
-    controls = new THREE.DeviceOrientationControls(camera, true);
-    controls.connect();
-    controls.update();
     // Mobile device full screen on click
     element.addEventListener('click', fullscreen, false);
-    // Controls object added, remove listener
-    window.removeEventListener('deviceorientation', setOrientationControls, true);
+    // Set up Canvas
+    canvas = document.getElementById('tempCanvas');
+    canvas.width = 512;
+    canvas.height = 288;
+    // Ensure it works nice as Three.js texture
+    canvas.width = nextPowerOf2(canvas.width);
+    canvas.height = nextPowerOf2(canvas.height);
+    function nextPowerOf2(x) { 
+        return Math.pow(2, Math.ceil(Math.log(x) / Math.log(2)));
+    }
+    // Set up context
+    context = canvas.getContext('2d');
+    texture = new THREE.Texture(canvas);
+    texture.context = context;
+    // Create cameraplane
+    var cameraPlane = new THREE.PlaneGeometry(512, 512);
+    // Create camera mesh
+    cameraMesh = new THREE.Mesh(cameraPlane, new THREE.MeshBasicMaterial({
+        color: 0xffffff, opacity: 1, map: texture
+    }));
+    cameraMesh.position.z = -200;
+    scene.add(cameraMesh);
+    // Drawing latest frame
+    animate();
 }
 
-function setOrientationControls(e) {
-    if (!e.alpha) {
-        return;
+function animate() {
+    if (context) {
+      var piImage = new Image();
+
+      piImage.onload = function() {
+        console.log('Drawing image');
+        context.drawImage(piImage, 0, 0, canvas.width, canvas.height);
+
+        texture.needsUpdate = true;
+      }
+      // Change ip when needed, determine from apache server host name
+      piImage.src = "http://" + window.location.hostname + "/html/cam_pic.php?time=" + new Date().getTime()
+    }
+
+    requestAnimationFrame(animate);
+
+    update();
+    render();
+}
+
+function resize() {
+    // fix size of projection
+    var width = container.offsetWidth;
+    var height = container.offsetHeight;
+    camera.aspect = width/height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(width,height);
+    effect.setSize(width,height);
+}
+
+function update(dt) {
+    resize();
+    camera.updateProjectionMatrix();
+}
+
+function render(dt) {
+    effect.render(scene, camera);
+}
+
+function fullscreen() {
+    if (container.requestFullscreen) {
+        container.requestFullscreen();
+    } else if (container.msRequestFullscreen) {
+        container.msRequestFullscreen();
+    } else if (container.mozRequestFullScreen) {
+        container.mozRequestFullScreen();
+    } else if (container.webkitRequestFullscreen) {
+        container.webkitRequestFullscreen();
     }
 }
