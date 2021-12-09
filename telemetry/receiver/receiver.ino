06d6ad31d0b4a7a9644396fe660b8d5248c45ab3
Lauren Xiong <78378477+Laurenx618@users.noreply.github.com>
2021-12-09 18:36:57 -0500

    changed telemetry data formats

--- telemetry/receiver/receiver.ino
+++ telemetry/receiver/receiver.ino
@@ -1,16 +1,13 @@
-#include <Wire.h>
-#include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_MS_PWMServoDriver.h"
-
-
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 RF24 radio(9, 10); // CE, CSN
-const byte address[6] = "00001";
+// NOTE: the "LL" at the end of the constant is "LongLong" type
+const uint64_t pipe = 0xE8E8F0F0E1LL; // Define the transmit pipe
 
-
-// Create the motor shield object with the default I2C address
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
@@ -25,26 +22,19 @@ int rightMotorSpeed=0;
 // Set up data recieve and use
 String readString = "";
 char lastIncomingChar;
+char newData[6];
 bool begin = false;
 int ind0;
 int ind1;
-int ind2;
 String left_motors = "";
 String right_motors = "";
 int lastDirection=1;
 int currentDirection;
 #define echoF 2 // attach pin D2 Arduino to pin Echo of HC-SR04
 #define trigF 3 //attach pin D3 Arduino to pin Trig of HC-SR04
-#define echoB 4 //SENSOR IN THE BACK
-#define trigB 5 //SENSOR IN THE BACK
-#define echoL 6 //SENSOR IN THE LEFT
-#define trigL 7 //SENSOR IN THE LEFT
-#define echoR 8 //SENSOR IN THE RIGHT
-#define trigR 9 //SENSOR IN THE RIGHT
+
 long distanceF;
-long distanceB;
-long distanceL;
-long distanceR;
+
 int Speed;
 unsigned long new_time;
 unsigned long next_time;
@@ -71,153 +61,108 @@ void setup() {
   rightMotor2->run(FORWARD);
   // Set up serial
   next_time = INTERVAL;       /* set time from reset */
-  pinMode( trigF, OUTPUT );
-  pinMode( trigB, OUTPUT );
-  pinMode( trigL, OUTPUT );
-  pinMode( trigR, OUTPUT );
-  digitalWrite( trigF, LOW );       // Set trig pin LOW here NOT later
-  digitalWrite( trigB, LOW );       // Set trig pin LOW here NOT later
-  digitalWrite( trigL, LOW );       // Set trig pin LOW here NOT later
-  digitalWrite( trigR, LOW );       // Set trig pin LOW here NOT later
-  pinMode( echoF, INPUT );
-  pinMode( echoB, INPUT );
-  pinMode( echoL, INPUT );
-  pinMode( echoR, INPUT );
-  Serial.begin( 9600 );
+  pinMode(trigF, OUTPUT);
+  digitalWrite(trigF, LOW);       // Set trig pin LOW here NOT later
+  pinMode(echoF, INPUT);
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("Nrf24L01 Receiver Starting");
+  radio.begin();
+  radio.openReadingPipe(1,pipe);
+  radio.startListening();
 }
 
 void loop() {
-  if (radio.available()){
-    while(true) {
-      new_time = millis( );
-        if( new_time >= next_time )
-        {
-          
-          digitalWrite( trigF, HIGH );
-          digitalWrite( trigB, HIGH );
-          digitalWrite( trigL, HIGH );
-          digitalWrite( trigR, HIGH );
-          delayMicroseconds( 10 );
-          digitalWrite( trigF, LOW );
-          digitalWrite( trigB, LOW );
-          digitalWrite( trigL, LOW );
-          digitalWrite( trigR, LOW );
-          distanceF = pulseIn( echoF, HIGH, MAX_ECHO );
-          distanceB = pulseIn( echoB, HIGH, MAX_ECHO );
-          distanceL = pulseIn( echoL, HIGH, MAX_ECHO );
-          distanceR = pulseIn( echoR, HIGH, MAX_ECHO );
-          if( distanceF > 0 ){
-          distanceF /= SCALE_CM;
-          }
-          if( distanceB > 0 ){
-          distanceB /= SCALE_CM;
-          }
-          if( distanceL > 0 ){
-          distanceL /= SCALE_CM;
-          }
-          if( distanceR > 0 ){
-          distanceR /= SCALE_CM;
-          }
-          
-          next_time = new_time + INTERVAL;   // save next time to run this part of loop
+  while(true) {
+    new_time = millis( );
+      if( new_time >= next_time )
+      {
+        
+        digitalWrite( trigF, HIGH );
+        delayMicroseconds( 10 );
+        digitalWrite( trigF, LOW );
+        distanceF = pulseIn( echoF, HIGH, MAX_ECHO );
+        if( distanceF > 0 ){
+        distanceF /= SCALE_CM;
         }
-          if(distanceF<5){
-            leftMotor1->run(BACKWARD);
-            leftMotor2->run(BACKWARD);
-            rightMotor1->run(BACKWARD);
-            rightMotor2->run(BACKWARD);
-          
-            Speed=30;
-            leftMotor1->setSpeed(Speed);
-            leftMotor2->setSpeed(Speed);
-            rightMotor1->setSpeed(Speed);
-            rightMotor2->setSpeed(Speed);
-            delay(700);
-          
-            leftMotor1->run(FORWARD);
-            leftMotor2->run(FORWARD);
-            rightMotor1->run(FORWARD);
-            rightMotor2->run(FORWARD);
-            leftMotor1->setSpeed(0);
-            leftMotor2->setSpeed(0);
-            rightMotor1->setSpeed(0);
-            rightMotor2->setSpeed(0);
-          }
-          if(distanceB<5){
-            leftMotor1->run(FORWARD);
-            leftMotor2->run(FORWARD);
-            rightMotor1->run(FORWARD);
-            rightMotor2->run(FORWARD);
-          
-            Speed=30;
-            leftMotor1->setSpeed(Speed);
-            leftMotor2->setSpeed(Speed);
-            rightMotor1->setSpeed(Speed);
-            rightMotor2->setSpeed(Speed);
-            delay(700);
-          
-            leftMotor1->run(FORWARD);
-            leftMotor2->run(FORWARD);
-            rightMotor1->run(FORWARD);
-            rightMotor2->run(FORWARD);
-            leftMotor1->setSpeed(0);
-            leftMotor2->setSpeed(0);
-            rightMotor1->setSpeed(0);
-            rightMotor2->setSpeed(0);
-          }
-          
-      if (Serial.available() > 0){
-        // Make sure data is at start
-        if (!begin){
-          lastIncomingChar = Serial.read(); // grab the most recent char
-          // when to start through ternary operator, make sure data format usable
-          begin = ( lastIncomingChar == '.' ) ? true : false;
+        
+        next_time = new_time + INTERVAL;   // save next time to run this part of loop
+      }
+        if(distanceF<5){
+          leftMotor1->run(BACKWARD);
+          leftMotor2->run(BACKWARD);
+          rightMotor1->run(BACKWARD);
+          rightMotor2->run(BACKWARD);
+        
+          Speed=30;
+          leftMotor1->setSpeed(Speed);
+          leftMotor2->setSpeed(Speed);
+          rightMotor1->setSpeed(Speed);
+          rightMotor2->setSpeed(Speed);
+          delay(700);
+        
+          leftMotor1->run(FORWARD);
+          leftMotor2->run(FORWARD);
+          rightMotor1->run(FORWARD);
+          rightMotor2->run(FORWARD);
+          leftMotor1->setSpeed(0);
+          leftMotor2->setSpeed(0);
+          rightMotor1->setSpeed(0);
+          rightMotor2->setSpeed(0);
         }
-        // Make sure that there is new data over Serial
-        // Note: needs to include '.', so not else if
-        if (begin) {
-          lastIncomingChar = Serial.read(); // grab the most recent char
-          // run when final index sent
-          if(lastIncomingChar == '*') {
-            ind0 = readString.indexOf('.'); // find initial index value
-            ind1 = readString.indexOf(','); // find index value of ',' in string
-            left_motors = readString.substring(ind0+1,ind1); // grab string from past '.' index to ','
-            right_motors = readString.substring(ind1+1); // grab string from index ',' on
-            leftMotorSpeed = left_motors.toInt(); // get integer value and store
-            rightMotorSpeed = right_motors.toInt();
-            // change speed
-            currentDirection=1;
-            if(leftMotorSpeed<0&&rightMotorSpeed<0){
-              currentDirection=0;
+        
+        
+    if (radio.available() > 0){
+      // Make sure data is at start
+      if (!begin){
+        lastIncomingChar = radio.read(newData, sizeof(newData)); // grab the most recent char
+        // when to start through ternary operator, make sure data format usable
+        begin = ( lastIncomingChar == '.' ) ? true : false;
+      }
+      // Make sure that there is new data over Serial
+      // Note: needs to include '.', so not else if
+      if (begin) {
+        lastIncomingChar = Serial.read(); // grab the most recent char
+        // run when final index sent
+        if(lastIncomingChar == '*') {
+          ind0 = readString.indexOf('.'); // find initial index value
+          ind1 = readString.indexOf(','); // find index value of ',' in string
+          left_motors = readString.substring(ind0+1,ind1); // grab string from past '.' index to ','
+          right_motors = readString.substring(ind1+1); // grab string from index ',' on
+          leftMotorSpeed = left_motors.toInt(); // get integer value and store
+          rightMotorSpeed = right_motors.toInt();
+          // change speed
+          currentDirection=1;
+          if(leftMotorSpeed<0&&rightMotorSpeed<0){
+            currentDirection=0;
+          }
+          if(currentDirection!=lastDirection){
+            if(currentDirection==1){
+              leftMotor1->run(FORWARD);
+              leftMotor2->run(FORWARD);
+              rightMotor1->run(FORWARD);
+              rightMotor2->run(FORWARD);
             }
-            if(currentDirection!=lastDirection){
-              if(currentDirection==1){
-                leftMotor1->run(FORWARD);
-                leftMotor2->run(FORWARD);
-                rightMotor1->run(FORWARD);
-                rightMotor2->run(FORWARD);
-              }
-              else{
-                leftMotor1->run(BACKWARD);
-                leftMotor2->run(BACKWARD);
-                rightMotor1->run(BACKWARD);
-                rightMotor2->run(BACKWARD);
-            }
+            else{
+              leftMotor1->run(BACKWARD);
+              leftMotor2->run(BACKWARD);
+              rightMotor1->run(BACKWARD);
+              rightMotor2->run(BACKWARD);
           }
-          leftMotorSpeed=abs(leftMotorSpeed);
-          rightMotorSpeed=abs(rightMotorSpeed);
-            leftMotor1->setSpeed(leftMotorSpeed);
-            leftMotor2->setSpeed(leftMotorSpeed);
-            rightMotor1->setSpeed(rightMotorSpeed);
-            rightMotor2->setSpeed(rightMotorSpeed);
-            // clear string to reuse
-            readString = "";
-            lastDirection=currentDirection;
-          }
-          else {
-            // build string starting from '.' until '*' char is reached
-            readString += lastIncomingChar;
-          }
+        }
+        leftMotorSpeed=abs(leftMotorSpeed);
+        rightMotorSpeed=abs(rightMotorSpeed);
+          leftMotor1->setSpeed(leftMotorSpeed);
+          leftMotor2->setSpeed(leftMotorSpeed);
+          rightMotor1->setSpeed(rightMotorSpeed);
+          rightMotor2->setSpeed(rightMotorSpeed);
+          // clear string to reuse
+          readString = "";
+          lastDirection=currentDirection;
+        }
+        else {
+          // build string starting from '.' until '*' char is reached
+          readString += lastIncomingChar;
         }
       }
     }
@@ -244,5 +189,24 @@ void loop(){
   }
   delay(5);
 }
- */
 
+
+ void loop()   /****** LOOP: RUNS CONSTANTLY ******/
+ /*
+{
+  if ( radio.available() )
+  {
+    // Read the data payload until we've received everything
+    bool done = false;
+    while (!done)
+    {
+      // Fetch the data payload
+      done = radio.read( RelaySwitch, sizeof(RelaySwitch) );
+      Serial.println(RelaySwitch[0]);
+    }
+  }
+  else
+  {    
+      Serial.println("No radio available");
+  }
+*/
